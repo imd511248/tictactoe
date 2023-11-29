8391b8e280f0dbf1576fe6d772140b2af88cb584
Irshad <imd511248@gmail.com>
2023-11-30 02:27:12 +0530

    add disableed function on button

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -4,6 +4,10 @@ import circleIcon from "../Assets/images/circles.png";
 import crossIcon from "../Assets/images/cros.png";
 let data = ["", "", "", "", "", "", "", "", ""];
 const TicTacToe = () => {
+  let [buttonDisabled, setButtonDisabled] = useState([false, false, false, false, false, false, false, false, false]);
+  let [count, setCount] = useState(0);
+  let [lock, setLock] = useState(false);
+
   let titleRef = useRef(null);
   let box1 = useRef(null);
   let box2 = useRef(null);
@@ -16,13 +20,18 @@ const TicTacToe = () => {
   let box9 = useRef(null);
   let array_box = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
 
-  let [count, setCount] = useState(0);
-  let [lock, setLock] = useState(false);
+  // let [isChecked] = useState(false);
 
   const toggle = (e, num) => {
-    if (lock) {
-      return 0;
+    console.log(e.target);
+    if (lock || buttonDisabled[num]) {
+      return;
     }
+    setButtonDisabled((prev) => {
+      const newDisabled = [...prev];
+      newDisabled[num] = true;
+      return newDisabled;
+    });
     if (count % 2 === 0) {
       e.target.innerHTML = `<img src="${crossIcon}" >`;
       data[num] = "x";
@@ -40,6 +49,7 @@ const TicTacToe = () => {
     setLock(false);
     titleRef.current.innerHTML = `Tic-Tac-Toe`;
     array_box.map((item) => (item.current.innerHTML = ""));
+    setButtonDisabled([false, false, false, false, false, false, false, false, false]);
   };
 
   const CheckWin = () => {
@@ -67,9 +77,9 @@ const TicTacToe = () => {
   const Won = (winner) => {
     setLock(true);
     if (winner === "x") {
-      titleRef.current.innerHTML = `Happy:<img src=${crossIcon}> win`;
+      titleRef.current.innerHTML = `Happy:<img src='${crossIcon}'> win`;
     } else {
-      titleRef.current.innerHTML = `Happy:<img src=${circleIcon}> win`;
+      titleRef.current.innerHTML = `Happy:<img src='${circleIcon}'> win`;
     }
   };
 
@@ -81,19 +91,19 @@ const TicTacToe = () => {
         </h1>
         <div className={Styles.container__board}>
           <div className={Styles.container__board__row1}>
-            <div className={Styles.container__board__row1__boxes} ref={box1} onClick={(e) => toggle(e, 0)}></div>
-            <div className={Styles.container__board__row1__boxes} ref={box2} onClick={(e) => toggle(e, 1)}></div>
-            <div className={Styles.container__board__row1__boxes} ref={box3} onClick={(e) => toggle(e, 2)}></div>
+            <button className={Styles.container__board__row1__boxes} ref={box1} disabled={buttonDisabled[0]} onClick={(e) => toggle(e, 0)}></button>
+            <button className={Styles.container__board__row1__boxes} ref={box2} disabled={buttonDisabled[1]} onClick={(e) => toggle(e, 1)}></button>
+            <button className={Styles.container__board__row1__boxes} ref={box3} disabled={buttonDisabled[2]} onClick={(e) => toggle(e, 2)}></button>
           </div>
           <div className={Styles.container__board__row2}>
-            <div className={Styles.container__board__row2__boxes} ref={box4} onClick={(e) => toggle(e, 3)}></div>
-            <div className={Styles.container__board__row2__boxes} ref={box5} onClick={(e) => toggle(e, 4)}></div>
-            <div className={Styles.container__board__row2__boxes} ref={box6} onClick={(e) => toggle(e, 5)}></div>
+            <button className={Styles.container__board__row2__boxes} ref={box4} disabled={buttonDisabled[3]} onClick={(e) => toggle(e, 3)}></button>
+            <button className={Styles.container__board__row2__boxes} ref={box5} disabled={buttonDisabled[4]} onClick={(e) => toggle(e, 4)}></button>
+            <button className={Styles.container__board__row2__boxes} ref={box6} disabled={buttonDisabled[5]} onClick={(e) => toggle(e, 5)}></button>
           </div>
           <div className={Styles.container__board__row3}>
-            <div className={Styles.container__board__row3__boxes} ref={box7} onClick={(e) => toggle(e, 6)}></div>
-            <div className={Styles.container__board__row3__boxes} ref={box8} onClick={(e) => toggle(e, 7)}></div>
-            <div className={Styles.container__board__row3__boxes} ref={box9} onClick={(e) => toggle(e, 8)}></div>
+            <button className={Styles.container__board__row3__boxes} ref={box7} disabled={buttonDisabled[6]} onClick={(e) => toggle(e, 6)}></button>
+            <button className={Styles.container__board__row3__boxes} ref={box8} disabled={buttonDisabled[7]} onClick={(e) => toggle(e, 7)}></button>
+            <button className={Styles.container__board__row3__boxes} ref={box9} disabled={buttonDisabled[8]} onClick={(e) => toggle(e, 8)}></button>
           </div>
         </div>
         <button
